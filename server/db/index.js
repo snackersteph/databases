476ea2a2a9d0f231cc23f510542d964b5ffe231a
Hack Reactor Student <student@hackreactor.com>
2017-09-18 19:55:01 -0700

    connect to mySQL

--- server/db/index.js
+++ server/db/index.js
@@ -3,5 +3,18 @@ var mysql = require('mysql');
 // Create a database connection and export it from this file.
 // You will need to connect with the user "root", no password,
 // and to the database "chat".
+var dbConnection = mysql.createConnection({
+  user: 'root',
+  database: 'chat',
+  password: 'plantlife'
+});
 
+dbConnection.connect(function(err) {
+  if (err) {
+    console.log('Error reaching database', err);
+  } else {
+    console.log('Connected to mySQL');
+  }
+});
 
+module.exports = dbConnection;
